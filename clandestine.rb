e0dd040d9298ebdc2d2e183c26984a87caae03f3
Ernest W. Durbin III <ewdurbin@gmail.com>
2014-07-05 12:35:06 -0400

    restructure, refactor, lint

---
+++ clandestine.rb
@@ -0,0 +1,88 @@
+require 'murmur3'
+require 'set'
+
+class RendezvousHash
+
+  def initialize(nodes=nil, hash_function=method(:murmur3_32_str_hash))
+    @nodes = nodes || []
+    @hash_function = hash_function
+  end
+
+  def add_node(node)
+    @nodes.push(node) unless @nodes.include?(node)
+  end
+
+  def remove_node(node)
+    @nodes.delete(node) if @nodes.include?(node)
+  end
+
+  def find_node(key)
+    @nodes.max {|a,b| @hash_function.call("#{a}-#{key}") <=> @hash_function.call("#{b}-#{key}")}
+  end
+
+end
+
+class Cluster
+
+  def initialize(cluster, hash_function=method(:murmur3_32_str_hash), replicas=2)
+    @hash_function = hash_function
+
+    @replicas = replicas
+    @nodes = Hash[]
+    @zones = []
+    @zone_members = Hash[]
+    @zone_members.default = []
+    @rings = Hash[]
+
+    cluster.each do |node, node_data|
+      name = node_data['name']
+      zone = node_data['zone']
+      add_zone(zone)
+      add_node(node, name, zone)
+    end
+  end
+
+  def add_zone(zone)
+    @zones.push(zone) unless @zones.include?(zone)
+    @zones.sort!
+  end
+
+  def remove_zone(zone)
+    @zones.delete(zone) if @zones.include?(zone)
+    @zones.sort!
+  end
+
+  def node_name_by_id(node_id)
+    @nodes[node_id]
+  end
+
+  def add_node(node_id, node_name=nil, node_zone=nil)
+    raise ArgumentError, 'Cluster not initialized with zone #{node_zone}' unless @zones.include?(node_zone)
+    unless @rings.has_key?(node_zone)
+      zone = RendezvousHash.new(nil, @hash_function)
+      @rings[node_zone] = zone
+    end
+    @rings[node_zone].add_node(node_id)
+    @nodes[node_id] = node_name unless @nodes.include?(node_id)
+    @zone_members[node_zone].push(node_id) unless @zone_members[node_zone].include?(node_id)
+  end
+
+  def remove_node(node_id, node_name=nil, node_zone=nil)
+    raise ArgumentError, 'Cluster not initialized with zone #{zone}' unless @zones.include?(zone)
+    @rings[node_zone].delete(node_id) if @rings.has_key?(node_zone)
+    @nodes.delete(node_id) if @nodes.include?(node_id)
+    @zone_members[node_zone].delete(node_id) if @zone_members[node_zone].include?(node_id)
+  end
+
+  def find_nodes(product_id, block_index)
+    nodes = []
+    offset = (product_id.to_i + block_index.to_i) % @zones.length
+    for i in (0...@replicas)
+      zone = @zones[(i + offset) % @zones.length]
+      key = "#{product_id}-#{block_index}"
+      nodes << @rings[zone].find_node(key)
+    end
+    nodes
+  end
+
+end
---
+++ murmur3.rb
@@ -0,0 +1,44 @@
+MASK32 = 0xffffffff
+
+def murmur3_32_rotl(x, r)
+  ((x << r) | (x >> (32 - r))) & MASK32
+end
+
+
+def murmur3_32_fmix(h)
+  h &= MASK32
+  h ^= h >> 16
+  h = (h * 0x85ebca6b) & MASK32
+  h ^= h >> 13
+  h = (h * 0xc2b2ae35) & MASK32
+  h ^ (h >> 16)
+end
+
+def murmur3_32__mmix(k1)
+  k1 = (k1 * 0xcc9e2d51) & MASK32
+  k1 = murmur3_32_rotl(k1, 15)
+  (k1 * 0x1b873593) & MASK32
+end
+
+def murmur3_32_str_hash(str, seed=0)
+  h1 = seed
+  numbers = str.unpack('V*C*')
+  tailn = str.length % 4
+  tail = numbers.slice!(numbers.size - tailn, tailn)
+  for k1 in numbers
+    h1 ^= murmur3_32__mmix(k1)
+    h1 = murmur3_32_rotl(h1, 13)
+    h1 = (h1*5 + 0xe6546b64) & MASK32
+  end
+
+  unless tail.empty?
+    k1 = 0
+    tail.reverse_each do |c1|
+      k1 = (k1 << 8) | c1
+    end
+    h1 ^= murmur3_32__mmix(k1)
+  end
+
+  h1 ^= str.length
+  murmur3_32_fmix(h1)
+end
