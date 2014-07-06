85d0e71ded84b2ba505b3e3738207bc08ebcda6f
Ernest W. Durbin III <ewdurbin@gmail.com>
2014-07-05 22:25:49 -0400

    murmur3 disclaimers

--- murmur3.rb
+++ murmur3.rb
@@ -1,3 +1,6 @@
+## MurmurHash3 was written by Austin Appleby, and is placed in the public
+## domain. The author hereby disclaims copyright to this source code.
+
 MASK32 = 0xffffffff
 
 def murmur3_32_rotl(x, r)
