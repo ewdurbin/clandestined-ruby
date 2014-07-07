66b9c760d7ee595e90f1a8adb5df21b62e62e1b2
Ernest W. Durbin III <ewdurbin@gmail.com>
2014-07-07 11:04:52 -0400

    ruby 1.8.6 fixup

--- lib/clandestined/cluster.rb
+++ lib/clandestined/cluster.rb
@@ -88,7 +88,7 @@ class Cluster
   def find_nodes(search_key, offset=nil)
     nodes = []
     unless offset
-      offset = search_key.split('').inject(0) { |s, c| s += c[0].ord }
+      offset = search_key.split('').inject(0) { |s, c| s += c[0,1].unpack('c')[0] }
     end
     for i in (0...replicas)
       zone = zones[(i + offset.to_i) % zones.length]
